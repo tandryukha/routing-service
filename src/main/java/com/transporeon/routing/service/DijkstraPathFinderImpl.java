c0766642d4f05af0884b79b59ea9447041b60d4b
ataran <andrew@1worldonline.com>
2022-08-07 12:37:29 +0300

    dijkstra prototype passes first test

--- src/main/java/com/transporeon/routing/service/DijkstraPathFinderImpl.java
+++ src/main/java/com/transporeon/routing/service/DijkstraPathFinderImpl.java
@@ -2,43 +2,55 @@ package com.transporeon.routing.service;
 
 import java.util.*;
 
+import static java.util.Collections.emptyList;
+
 /**
  * Customized and tweaked to the interface Dijkstra shortest path algorithm
  */
 public class DijkstraPathFinderImpl implements PathFinder {
 
-//todo handle when there is no path
     //todo stop when destination reached
     //todo stop when maxStops reached
 @Override
 public <T> Optional<List<T>> findShortestPath(Map<T, List<Node<T>>> adjacencyList, T source, T dest, int maxStops) {
     PriorityQueue<Node<T>> heap = new PriorityQueue<>(adjacencyList.size());
-    HashMap<T, Double> distance = new HashMap<>(adjacencyList.size());//todo store in node
+    HashMap<T, Double> distance = new HashMap<>(adjacencyList.size());
     Set<T> visited = new HashSet<>(adjacencyList.size());
-    HashMap<T, T> shortestPath = new HashMap<>(adjacencyList.size());
-    shortestPath.put(source, null);
+    HashMap<T, T> paths = new HashMap<>(adjacencyList.size());
+    paths.put(source, null);
     // Add source node to the priority queue
     heap.add(new Node<>(source, 0));
-
     // Distance to the source is 0
     distance.put(source, 0d);
-    while (visited.size() != adjacencyList.size() && !heap.isEmpty()) {
+    while (!heap.isEmpty()) {
         // Removing the minimum distance node from the priority queue
         Node<T> closestNode = heap.remove();
-        visited.add(closestNode.value());//todo store visited in node itself
-        for (Node<T> node : adjacencyList.get(closestNode.value())) {
+        visited.add(closestNode.value());
+        for (Node<T> node : adjacencyList.getOrDefault(closestNode.value(), emptyList())) {
             if (visited.contains(node.value())) continue;
             double distanceToAdjNode = node.distance() + distance.get(closestNode.value());
             // If current route is closer than what we already have
             if (distanceToAdjNode < distance.getOrDefault(node.value(), Double.POSITIVE_INFINITY)) {
                 distance.put(node.value(), distanceToAdjNode);
-                shortestPath.put(node.value(), closestNode.value());//todo store in node
+                paths.put(node.value(), closestNode.value());
             }
             heap.add(node);
         }
     }
 
-    //todo result
-
+    return getShortestPath(source, dest, paths);
 }
+
+    private static <T> Optional<List<T>> getShortestPath(T source, T dest, HashMap<T, T> paths) {
+        if (!paths.containsKey(dest)) return Optional.empty();
+        LinkedList<T> shortestPath = new LinkedList<>();
+        shortestPath.add(dest);
+        T curr = dest;
+        while (curr != source) {
+            T prevNode = paths.get(curr);
+            shortestPath.addFirst(prevNode);
+            curr = prevNode;
+        }
+        return Optional.of(shortestPath);
+    }
 }
