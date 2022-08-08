0f2e90af83bee4f87fc6b3610ebf8d25e152eaf8
ataran <andrew@1worldonline.com>
2022-08-08 17:27:46 +0300

    DP path finder tests pass

--- src/main/java/com/transporeon/routing/service/DPPathFinder.java
+++ src/main/java/com/transporeon/routing/service/DPPathFinder.java
@@ -5,36 +5,45 @@ import java.util.*;
 import static java.util.Collections.emptyList;
 
 public class DPPathFinder implements PathFinder {
-    @Override
+    @Override//todo avoid optional in contract - use empty list if shortest path not found
     public <T> Optional<List<T>> findShortestPath(Map<T, List<Node<T>>> adjacencyList, T source, T dest, int maxHops) {
-        //todo edge case handling can be copied from DijkstraPathFinder
-        List<T> result = findShortestPathRec(adjacencyList, source, dest, maxHops);
-        return Optional.empty();
+        if (adjacencyList.isEmpty() || maxHops < 1) return Optional.empty();
+        List<Node<T>> result = findShortestPathRec(adjacencyList, source, dest, maxHops);
+        if (result.isEmpty()) return Optional.empty();
+        return Optional.of(result.stream().map(Node::getValue).toList());
     }
 
-    private <T> List<T> findShortestPathRec(Map<T, List<Node<T>>> adjacencyList, T source, T dest, int hops) {
-        if (source == dest) return List.of(source);//base case
+    //todo is it possible to memoize results? take a look at the knapsack problem
+    private <T> List<Node<T>> findShortestPathRec(Map<T, List<Node<T>>> adjacencyList, T source, T dest, int hops) {
+        if (source == dest) return toLinkedList(new Node<>(dest, 0d));//base case
         if (hops == 0) return emptyList();//base case
 
-
-        List<Node<T>> adjacentNodes = adjacencyList.get(source);
-        double minDistance = Double.POSITIVE_INFINITY;
-        List<T> shortestPath = emptyList();
+        List<Node<T>> adjacentNodes = adjacencyList.getOrDefault(source,emptyList());
+        double minDistanceToDest = Double.POSITIVE_INFINITY;
+        List<Node<T>> shortestPath = emptyList();
+        double distanceToAdjacentNode = Double.POSITIVE_INFINITY;
         for (Node<T> adjacentNode : adjacentNodes) {
-            List<T> path = findShortestPathRec(adjacencyList, adjacentNode.getValue(), dest, hops - 1);
+            List<Node<T>> path = findShortestPathRec(adjacencyList, adjacentNode.getValue(), dest, hops - 1);
             if (path.isEmpty()) continue;
-            double distance = getDistance(path);
-            if (distance < minDistance) {
-                minDistance = distance;
+            double distanceToDest = getDistance(path);
+            if (distanceToDest < minDistanceToDest) {
+                minDistanceToDest = distanceToDest;
                 shortestPath = path;
+                distanceToAdjacentNode = adjacentNode.getDistance();
             }
         }
         if (shortestPath.isEmpty()) return emptyList();
-        shortestPath.add(0, source);
+        shortestPath.add(0, new Node<>(source, distanceToAdjacentNode));
         return new LinkedList<>(shortestPath);
     }
 
-    private <T> double getDistance(List<T> path) {
-        //todo
+    private <T> List<Node<T>> toLinkedList(Node<T> node) {
+        LinkedList<Node<T>> nodes = new LinkedList<>();
+        nodes.add(node);
+        return nodes;
+    }
+
+    private <T> double getDistance(List<Node<T>> path) {
+        return path.stream().map(Node::getDistance).reduce(Double::sum).orElse(Double.NEGATIVE_INFINITY);
     }
 }
